829f9467557f750e996d99d32734f0894f4d52bd
alexandersong <alexfsong@gmail.com>
2020-03-06 13:33:50 -0500

    kl

--- app/src/main/java/com/songalexander/layoutapp/Main6Activity.java
+++ app/src/main/java/com/songalexander/layoutapp/Main6Activity.java
@@ -5,20 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ScrollView;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.TextView;
 
-public class Main5Activity extends AppCompatActivity {
+public class Main6Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main5);
-        TextView sc = findViewById(R.id.scroll);
-        sc.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main6);
+        Button click = findViewById(R.id.click);
+        click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Main5Activity.this, Main6Activity.class));
+                startActivity(new Intent(Main6Activity.this, Main7Activity.class));
             }
         });
     }
